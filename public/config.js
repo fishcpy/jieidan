e13d34c495c20c1ec8879d2b08b17cfb22ce3e5b
fishcpy <140036207+fishcpy@users.noreply.github.com>
2024-09-18 21:11:16 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,12 +1,15 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: '博客检点检测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2675035-4b6533c03700aa8da0c5c9e4',
+    'm797697117-f32233d03be773c50e92bef0',
+    'm797697139-9798fd3b5ee6b0f66c7aaddd',
+    'm797697159-353615572abe099a19729d9b',
+    'm797697132-5c4577bd2a673a3ba06e9465',
   ],
 
   // 日志天数
